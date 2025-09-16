e3a053935b882e9a6e58a370c14f369b0303e9bb
SkogisAB <filip.skoglund@elev.ntig.se>
2025-09-16 11:20:35 +0200

    Lektion2 - Lagt till specialattack med tillhörande

--- src/Main.java
+++ src/Main.java
@@ -23,16 +23,28 @@ public class Main {
         stats2.add(randomgenerator.nextInt(21, 80));
 
         // Print character stats to check if it works
-        System.out.println("Character 1 has " + stats1.get(0) + " hp.");
-        System.out.println("Character 2 has " + stats2.get(0) + " hp.");
+        System.out.println("Character 1 has " + stats1.getFirst() + " hp.");
+        System.out.println("Character 2 has " + stats2.getFirst() + " hp.");
 
 
         // Initialize standard attack (smakareu attack)
-        int newhp = standardAttack(stats1.get(2), stats2.get(0));
+        int newhp = standardAttack(stats1.get(2), stats2.getFirst());
         stats2.set(0, newhp);
-        System.out.println("Character 1 hit character two, \nThey now only have " + newhp + "hp left");
+        System.out.println("Character 1 hit character 2, \nThey now only have " + newhp + "hp left");
 
+        // Initialize special attack (Supersmakaerueu attackish)
+        int[] hpMana = specialAttack(stats2.get(2), stats2.get(1), stats1.getFirst());
+        stats1.set(0, hpMana[0]);
+        stats2.set(1, hpMana[1]);
+        System.out.println("Charcter 2 hit character 1, \nThey now only have " + hpMana[0] + "hp left");
+        System.out.println("Character 2 now has " + stats2.get(1) + " mana left");
+
+        // TODO: Fix game loop in seperate method
+        //          Start with user interaction
+        //          After let AI hit back with random choice
+        //          If time:  Make AI more clever in attack choice
     }
+
 
     // Method for standard attack
     public static int standardAttack(int damage, int hp) {
@@ -48,12 +60,19 @@ public class Main {
     }
 
     // Method for special attack
-    public static void specialAttack() {
+    public static int[] specialAttack(int damage, int mana, int hp) {
 
+        if (mana - 20 < 0) {
+            System.out.println("The special attack isn't available. You need more mana.");
+        } else {
+            // utför attacken
+            hp = (int) (hp - 0.25 * damage);
+            mana -= 20;
+        }
+
+        int[] returnArray = {hp, mana};
+        return returnArray;
     }
 
 
-    /*
-    TODO: fix special attack
-     */
 }
