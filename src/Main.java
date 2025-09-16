ee65c30a87253119c7bb94d67dd8da1615b989fd
SkogisAB <filip.skoglund@elev.ntig.se>
2025-09-16 10:18:46 +0200

    hejhej

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static Random randomgenerator = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to my fighting game");
+
+        // Contains the stats of the first character (hp, mana, damage)
+        ArrayList<Integer> stats1 = new ArrayList<>();
+        stats1.add(100);
+        stats1.add(randomgenerator.nextInt(21, 80));
+        stats1.add(randomgenerator.nextInt(21, 80));
+
+        // Contains the stats of the second character (hp, mana, damage)
+        ArrayList<Integer> stats2 = new ArrayList<>();
+        stats2.add(100);
+        stats2.add(randomgenerator.nextInt(21, 80));
+        stats2.add(randomgenerator.nextInt(21, 80));
+
+        // Print character stats to check if it works
+        System.out.println("Character 1 has " + stats1.get(0) + " hp.");
+        System.out.println("Character 2 has " + stats2.get(0) + " hp.");
+
+
+        // Initialize standard attack (smakareu attack)
+        int newhp = standardAttack(stats1.get(2), stats2.get(0));
+        stats2.set(0, newhp);
+        System.out.println("Character 1 hit character two, \nThey now only have " + newhp + "hp left");
+
     }
+
+    // Method for standard attack
+    public static int standardAttack(int damage, int hp) {
+        int chance = randomgenerator.nextInt(100); // generates a number 0-99
+        if (chance < 67) {
+            hp = hp - damage / 6;
+            System.out.println("Regular hit");
+        } else {
+            hp = hp - damage;
+            System.out.println("Critical hit");
+        }
+        return hp;
+    }
+
+    // Method for special attack
+    public static void specialAttack() {
+
+    }
+
+
+    /*
+    TODO: fix special attack
+     */
 }
